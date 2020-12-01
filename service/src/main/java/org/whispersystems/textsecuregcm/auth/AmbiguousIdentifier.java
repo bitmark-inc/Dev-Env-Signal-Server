e38018d2ca017f35a674d735d1a2aa3b81439911
Christopher Hall <hsw@bitmark.com>
2020-12-01 14:24:33 +0800

    allow exact match of UUID to succeed
    otherwise treat everything else as a number
    
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

--- service/src/main/java/org/whispersystems/textsecuregcm/auth/AmbiguousIdentifier.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/auth/AmbiguousIdentifier.java
@@ -8,14 +8,24 @@ public class AmbiguousIdentifier {
   private final String number;
 
   public AmbiguousIdentifier(String target) {
-    // if (target.startsWith("+")) {
+    if (target.matches("^[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}$")) {
+      this.uuid   = UUID.fromString(target);
+      this.number = null;
+    } else {
       this.uuid   = null;
       this.number = target;
-    // } else {
-    //   this.uuid   = UUID.fromString(target);
-    //   this.number = null;
-    // }
+    }
   }
+
+  // public AmbiguousIdentifier(String target) {
+  //   if (target.startsWith("+")) {
+  //     this.uuid   = null;
+  //     this.number = target;
+  //   } else {
+  //     this.uuid   = UUID.fromString(target);
+  //     this.number = null;
+  //   }
+  // }
 
   public UUID getUuid() {
     return uuid;
